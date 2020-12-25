6f3d126122c7b3dadd4090e6afc864719f03700f
http1ucc4s <http1ucc4s@protonmail.com>
2020-12-24 23:07:33 -0300

    Routes implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import Panel from './views/Panel/Panel'
+import Login from './views/Login/Login'
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <Panel></Panel>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact={true} component={Login} />
+        <Route path="/panel" component={Panel} />   
+      </Switch>
+    </BrowserRouter>
   );
 }
 
