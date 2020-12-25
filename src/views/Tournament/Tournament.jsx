67ded50084d80857d2df922e0ce5b3d378d76962
http1ucc4s <http1ucc4s@protonmail.com>
2020-12-25 14:25:19 -0300

    Added SweetAlert2 and improved tournament

--- src/views/Tournament/Tournament.jsx
+++ src/views/Tournament/Tournament.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap'
 import NavbarComponent from '../../components/Navbar'
+import DatePickerComponent from '../../components/DatePicker'
 
 const TournamentPage = () => {
     return (
         <Container>
             <NavbarComponent />
-
+            <DatePickerComponent></DatePickerComponent>
         </Container>
     )
 }
